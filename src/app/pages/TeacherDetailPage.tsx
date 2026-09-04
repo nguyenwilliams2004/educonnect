1a70e6babe5d5f6acc3b226aa4e65a36e06bca88
Nguyen Nhat Nguyen <nguyennhatnguyen2304@gmail.com>
2026-09-04 16:34:14 +0700

    fix(teacher-detail): import useMemo to resolve white screen ReferenceError

--- src/app/pages/TeacherDetailPage.tsx
+++ src/app/pages/TeacherDetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Link, useParams } from 'react-router';
 import {
   CheckCircle,
